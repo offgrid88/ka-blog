ead2af836329d749f6e3c22d29636e17bf6bdcea
Aymen.rachdi <acsdogs69@gmail.com>
2023-07-04 16:02:34 +0100

    auth

--- services/web/app/routes.py
+++ services/web/app/routes.py
@@ -4,19 +4,17 @@ import smtplib, ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.utils import formatdate
-from flask import render_template, flash, redirect, url_for, request, session,Flask,jsonify
+from flask import Flask, render_template, flash, redirect, url_for, request, session
 from app import app
 from dotenv import load_dotenv, find_dotenv
 import os
 from bson import ObjectId
 from datetime import datetime
 from pymongo import MongoClient
-from flask_jwt_extended import JWTManager, create_access_token, get_jwt_identity, jwt_required
 import datetime
-import hashlib
+import bcrypt, hashlib
 import urllib
-
-
+from flask_login import LoginManager ,login_user,login_required,logout_user,current_user,UserMixin
 
 
 #######################################################
@@ -65,7 +63,8 @@ password= urllib.parse.quote_plus(os.environ.get("MONGO_PWD"))
 
 username = urllib.parse.quote_plus(os.environ.get("USER_NAME"))
 
-
+app.secret_key = 'dfdfdsfslvksfùvvùlsfvl;fsùv;ùfsv'
+app.config['SESSION_TYPE'] = 'filesystem'
 
 
 db= MongoClient('offgrid8_db', 27018, username=username, password=password,authSource="admin")
@@ -80,9 +79,87 @@ booksDB = databaseBooks()
 postsDB = databaseArticles()
 users_collection = mydb["users"]
 
-jwt = JWTManager(app) # initialize JWTManager
-app.config['JWT_SECRET_KEY'] = '38dd56f56d405e02ec0ba4be4607eaab'
-app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(days=1) # define the life span of the token
+
+@app.route("/register", methods=['post', 'get'])
+def register():
+    message = ''
+    #if method post in index
+    if "email" in session:
+        return redirect(url_for("add"))
+    if request.method == "POST":
+        username = request.form.get("user")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        psw = request.form.get("verif_pass")
+        #if found in database showcase that it's found 
+        username_found = users_collection.find_one({"name": username})
+        email_found = users_collection.find_one({"email": email})
+        if username_found:
+            message = 'There already is a username by that name'
+            return render_template('register.html', message=message)
+        if email_found:
+            message = 'This email already exists in database'
+            return render_template('register.html', message=message)
+        if password != psw:
+            message = 'Passwords should match!'
+            return render_template('register.html', message=message)
+        else:
+            #hash the password and encode it
+            hashed = bcrypt.hashpw(psw.encode('utf-8'), bcrypt.gensalt())
+            #assing them in a dictionary in key value pairs
+            username_input = {'name': username, 'email': email, 'password': hashed}
+            #insert it in the record collection
+            users_collection.insert_one(username_input)
+            
+            #find the new created account and its email
+            username_data = users_collection.find_one({"email": email})
+            new_email = username_data['email']
+            #if registered redirect to logged in as the registered username
+            return render_template('add.html', email=new_email)
+    return render_template('register.html')
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    message = 'Please login to your account'
+    if "email" in session:
+        return redirect(url_for("add"))
+
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+        #check if email exists in database
+        email_found = users_collection.find_one({"email": email})
+        if email_found:
+            email_val = email_found['email']
+            passwordcheck = email_found['password']
+            #encoded_passwordcheck=passwordcheck.encode('utf-8')
+            hashed_passwordcheck=bcrypt.hashpw(passwordcheck, bcrypt.gensalt())
+            #encode the password and check if it matches
+            if bcrypt.checkpw(password.encode('utf-8'), hashed_passwordcheck):
+                session["email"] = email_val
+                return redirect(url_for('add'))
+            else:
+                if "email" in session:
+                    return redirect(url_for("add"))
+                message = 'Wrong password'
+                return render_template('login.html', message=message)
+        else:
+            message = 'Email not found'
+            return render_template('login.html', message=message)
+    return render_template('login.html', message=message)
+
+
+
+@app.route("/logout", methods=["POST", "GET"])
+def logout():
+    if "email" in session:
+        session.pop("email", None)
+        return render_template("signout.html")
+    else:
+        return render_template('index.html')
+    
+
 
 
 @app.route('/')
@@ -97,62 +174,6 @@ def articles():
     print(allPosts)
     return render_template('articles.html', posts = allPosts)
 
-
-
-@app.route("/register", methods=["GET","POST"])
-def register():
-    if request.method == "POST":
-        allUsers = mydb.users.find({})
-        userMails = [user["email"].lower() for user in allUsers]
-        data = request.form
-        email = data["email"]
-        username = data["user"]
-        psw = data["password"]
-        verif = data["verif_pass"]
-        new_user = {"email": email, "username": username, "password": psw}
-        if psw != verif:
-            error = "Password Mismatch !"
-            return render_template('register.html', data=error)
-        elif email.lower() in userMails:
-            error = "Email address already exists !"
-            return render_template('register.html', data=error)
-        else:
-            # Creating Hash of password to store in the database
-            new_user["password"] = hashlib.sha256(new_user["password"].encode("utf-8")).hexdigest() # encrpt password
-            inserted_id=users_collection.insert_one(new_user)
-            if inserted_id:
-                return render_template('login.html', data=["green","Successfully Added, Please Log In"])
-    else:
-        return render_template('register.html')
-
-@app.route("/login", methods=["GET","POST"])
-def login():
-    if request.method == "POST":
-        login_details = request.form
-#        email = data["email"]
-#        psw = data["password"]
-        allUsers = mydb.users.find({})
-        # Checking if user exists in database or not
-        user_from_db = users_collection.find_one({'username': login_details['username']})  # search for user in database
-        # If user exists
-        if user_from_db:
-            # Check if password is correct
-            encrpted_password = hashlib.sha256(login_details['password'].encode("utf-8")).hexdigest()
-            if encrpted_password == user_from_db['password']:
-                # Create JWT Access Token
-                access_token = create_access_token(identity=user_from_db['username']) # create jwt token
-                # Return Token
-                return jsonify(access_token=access_token), 200
-            else:
-                return jsonify({'msg': 'The username or password is incorrect'})
-        
-        for user in allUsers:
-            if (user["email"] == email) and (user["password"] == psw):
-                return render_template("add.html")
-        return render_template('login.html', data=["red","Wrong Credentials !"])
-
-    else:
-        return render_template('login.html', data = ["",""])
 
 @app.route('/books')
 def books():
@@ -178,19 +199,25 @@ def getCurrentDateTime():
 def about():
     return render_template('about.html')
 
+        
+    
 @app.route('/add',methods=['GET','POST'])
 def add():
-    if request.method == "GET":
-        return render_template("add_article.html")
-    else:
-        json = request.json
-        inserted_id=postsDB.insert_one(json).inserted_id
-        print("success")
-        if inserted_id:
-            print("success")
-            return render_template('fullpost.html', post = inserted_id)
+    if "email" in session:
+        email = session["email"]
+        if request.method == "GET":
+            return render_template('add.html', email=email)
         else:
-            print("failed")
+            json = request.json
+            inserted_id=postsDB.insert_one(json).inserted_id
+            print("success")
+            if inserted_id:
+                print("success")
+                return render_template('fullpost.html', post = inserted_id)
+            else:
+                print("failed")
+    else:
+        return redirect(url_for("login"))
 
 
 
