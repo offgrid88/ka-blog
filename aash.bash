22b4b95effb36ba0d72e43ea4f15685bd277ab5b
Aymen.rachdi <acsdogs69@gmail.com>
2023-07-10 16:21:55 +0100

    markdown change

--- aash.bash
+++
@@ -1,1 +0,0 @@
-rsync -rtvzP --exclude  'apacheconf/apache2' --exclude '.git' --exclude 'database'  --exclude 'venv' --exclude '__pycache__'  /home/offgrid8/ka-blog/ offgrid8@107.175.76.231:/home/offgrid8/ka-blog
--- services/web/wsgi.py
+++ services/web/wsgi.py
@@ -1,5 +1,48 @@
-from app import app
+import sys
+from flask import Flask, render_template
+from flask_flatpages import FlatPages, pygments_style_defs
+from flask_frozen import Freezer
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000,debug=True,threaded=True)
 
+DEBUG = True
+FLATPAGES_AUTO_RELOAD = DEBUG
+FLATPAGES_EXTENSION = '.md','.html'
+FLATPAGES_ROOT = 'content'
+POST_DIR = 'posts'
+
+app = Flask(__name__)
+flatpages = FlatPages(app)
+freezer = Freezer(app)
+app.config['FREEZER_IGNORE_MIMETYPE_WARNINGS']= True
+app.config.from_object(__name__)
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/books/')
+def books():
+    return render_template('books.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route("/posts/")
+def posts():
+    posts = [p for p in flatpages if p.path.startswith(POST_DIR)]
+    posts.sort(key=lambda item:item['date'], reverse=False)
+    return render_template('posts.html', posts=posts)
+
+@app.route('/posts/<name>/')
+def post(name):
+    path = '{}/{}'.format(POST_DIR, name)
+    post = flatpages.get_or_404(path)
+    return render_template('post.html', post=post)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1 and sys.argv[1] == "build":
+        freezer.freeze()
+    else:
+        app.run(host='127.0.0.1', port=5000,debug=True,threaded=True)
